155e1fe958fedf8906584ed6930a7e94ebc37d0b
ostyyy <ggg468432@gmail.com>
2026-04-25 23:00:42 +0300

    Add findExpression function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function findExpression(value) {
+    function tryPath(current, expression) {
+        if (current === value) return expression;
+        if (current > value) return null;
+
+        return (
+            tryPath(current * 3, `(${expression}*3)`) ||
+            tryPath(current + 5, `(${expression}+5)`)
+        );
+    }
+    return tryPath(1, '1');
+}
+
+console.log(findExpression(13));
